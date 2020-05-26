481fa21fa468980375725133c3d8ffc7972c46ad
Alioune <alioune1117@gmail.com>
2020-05-26 19:33:59 +0200

    completato l'esercizio , ma errore nella logica nel caso il cognome faccia già parte del database iniziale.

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var cognomeUtente = prompt("Inserisci il tuo cognome");
 console.log('Cognome inserito dall utente: '+cognomeUtente);
 
 // Inserisco il suo cognome all'interno del database
-nuovoUtente = listaCognomi.push(cognomeUtente);
+listaCognomi.push(cognomeUtente);
 console.log('Nuova lista con cognome dell utente: '+listaCognomi);
 
 // Stampo la lista ordinata alfabeticamente
@@ -15,30 +15,32 @@ var ordineAlfabetico = listaCognomi.sort();
 
 console.log('Lista cognomi ordinati alfabeticamente: '+ordineAlfabetico);
 
+var messaggio
 
-// Scrivo la posizione del cognome del nuovo utente
+
+// Controllo se il cognome è già inserito nel database
 
 var verifica = false;
-
 var i = 0;
 
-while ((verifica == false) && (i < listaCognomi.length)) {
-
-  // nuovoUtente == cognomeUtente
-
-
-  if (nuovoUtente == cognomeUtente) {
+while ((verifica) && (i < listaCognomi.length))  {
+  if ( cognomeUtente == listaCognomi[i] ) {
     verifica = true;
+
 
   }
 
-
   i++;
-
-
 }
 
+// Stampo l'esito e comunico il numero di posizionamento all'utente
 
-if (verifica == true) {
-  console.log(listaCognomi[i]);
+if (verifica) {
+  messaggio = "il tuo cognome è già nel database";
+  console.log(messaggio);
+
+} else {
+  var numeroCognomeUtente = parseInt(listaCognomi.indexOf(cognomeUtente))
+  messaggio = "La posizione nella lista del tuo cognome è nr. " + (numeroCognomeUtente + 1);
+  console.log(messaggio);
 }
